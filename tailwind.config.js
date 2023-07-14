0125621acaaac8f608b93a4bc3a1034648e59b4d
xDamneDx <xdamnedx@gmail.com>
2023-07-14 12:08:31 +0200

    add creations section w/ gradient overlays

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         sans: ["Josefin Sans", "sans-serif"],
         alata: ["Alata", "sans-serif"],
       },
+      letterSpacing: {
+        widest: ".3em",
+      },
     },
   },
   plugins: [],
